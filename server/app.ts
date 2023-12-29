83df2e3107c935f5a12f5475bed271502f867c1a
Pawan Maurya <y2k2anomaly@gmail.com>
2023-12-29 11:56:23 +0530

    :/NA

--- server/app.ts
+++ server/app.ts
@@ -22,14 +22,6 @@ app.use(
     })
 );
 
-// middlewares to enable CORS for all routes
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT as string);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
 // routes
 app.use("/api/v1", userRouter);
 app.use("/api/v1", notesRouter);
