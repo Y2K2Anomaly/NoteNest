23986d740b87173762fb397e3b4c883d2068ba65
Pawan Maurya <y2k2anomaly@gmail.com>
2023-12-30 00:34:15 +0530

    :/NA

--- server/app.ts
+++ server/app.ts
@@ -7,20 +7,33 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route";
 import { ErrorMiddleware } from "./middleware/error";
 import notesRouter from "./routes/notes.route";
+import helmet from "helmet";
 
 // body parser
 app.use(express.json({ limit: "50mb" }));
 
 // cookie parser
 app.use(cookieParser());
+app.use(helmet());
 
 // cors => cross origin resource sharing
 app.use(
     cors({
-        origin: "*",
+        origin: process.env.CLIENT,
         credentials: true,
     })
 );
+
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+// middlewares to enable CORS for all routes
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT as string);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 // routes
 app.use("/api/v1", userRouter);
