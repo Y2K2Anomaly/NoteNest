11321fdcaf6dccd6c4e4b748c4abc459f2f24c4f
Pawan Maurya <y2k2anomaly@gmail.com>
2023-12-30 18:11:56 +0530

    UI fix

--- client/app/styles/style.ts
+++ client/app/styles/style.ts
@@ -2,5 +2,5 @@ export const styles = {
     option_button: "text-gray-400 hover:text-red-600 hover:bg-slate-900 w-full h-full px-2 py-4 transform transition delay-150 duration-150 ease-out cursor-pointer",
     hover_circle: "p-2 active:scale-95 rounded-full hover:bg-slate-600",
     edit_save_button: "w-16 h-10 text-white self-end ml-[940px]",
-    note_body: "bg-white rounded-xl w-[1000px] h-[400px] z-100 mx-auto absolute top-[60%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] shadow-lg drop-shadow-md",
+    note_body: "bg-white rounded-xl sm:w-[400px] md:w-[600px] lg:w-[800px] xl:w-[1000px] h-[400px] z-100 mx-auto absolute top-[60%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] shadow-lg drop-shadow-md",
 };
