5ccc63f5e6ff00a68f516d7784b22339923ac508
Pawan Maurya <y2k2anomaly@gmail.com>
2023-12-30 16:49:13 +0530

    fixed deployment issue

--- server/app.ts
+++ server/app.ts
@@ -19,7 +19,7 @@ app.use(helmet());
 // cors => cross origin resource sharing
 app.use(
     cors({
-        origin: "https://notenest01.vercel.app",
+        origin: process.env.CLIENT,
         credentials: true,
     })
 );
@@ -28,10 +28,23 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
 // middlewares to enable CORS for all routes
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', "https://notenest01.vercel.app");
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header("Access-Control-Allow-Credentials", "true");
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        process.env.CLIENT as string
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Private-Network", "true");
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
+
     next();
 });
 
